d1daae6df762ebab4ced1ee4fe06316f8b7ada9e
aduro005 <130402134+aduro005@users.noreply.github.com>
2023-06-13 13:54:10 -0700

    Step 12 in HSI Data Processing
    Make pga.RData (wide) and pgamelt.RData (long) data frames containing the observed (uncorrected) reflectance intensity spectrum for each sample at each configuration

--- HSICalLib_7a_observedI.R
+++ HSICalLib_7a_observedI.R
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------------
 # Objective: Observed & melted obs for each spectrum
 # Author: Alyssa M. Duro
-# Last edited: 4/10/2023
+# Last edited: 6/13/2023
 # ---------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------
@@ -21,46 +21,18 @@ load("../Output Files/HSICalLib_20230223_b1-b30_p_gold_acor.RData")
 load("../Output Files/HSICalLib_wavevec.RData")
 
 # ---------------------------------------------------------------------------
-# melt p_gold_acor
-# ---------------------------------------------------------------------------
-
-datatable <- p_gold_acor[,c(3,13:485)]
-
-names(datatable)[4:474] <- wavevec
-
-melt2 <- as.data.table(datatable)
-melt1 <- melt(melt2,
-              id.vars=colnames(melt2)[c(1:3)],
-              measure.vars=colnames(melt2)[4:474],
-              variable.name="wavelength", 
-              value.name="obsI") 
-
-# make wavelength numeric
-melt1$wavelength <- as.numeric(as.character(melt1$wavelength))
-
-p_gold_acor_melt <- melt1
-
-rm(list=c('melt1','melt2','datatable'))
-
-save ( list = c ( 'p_gold_acor_melt' ) , 
-       file = paste ("../Output Files/HSICalLib_20230223_b1-b30_p_gold_acor_melt.RData", 
-                     sep="") )
-
-# Load file that was just output
-load("../Output Files/HSICalLib_20230223_b1-b30_p_gold_acor_melt.RData")
-
-# ---------------------------------------------------------------------------
 # remove na's from p_gold_acor
 # ---------------------------------------------------------------------------
 
-pga <- p_gold_acor[which(!is.na(p_gold_acor[,15])),]
-pga <- pga[which(!is.na(pga$batch)),]
+# remove any rows with missing spectra --> 99,537 
+pga <- p_gold_acor[which(!is.na(p_gold_acor[,15])),] # waveband 1
+pga <- pga[which(!is.na(pga$HSInumber)),]
 
 save ( list = c ( 'pga' ) , 
        file = paste ("../Output Files/HSICalLib_20230223_b1-b30_pga.RData", 
                      sep="") )
 
-load("../Output Files/HSICalLib_20230223_b1-b30_pga.RData",)
+load("../Output Files/HSICalLib_20230223_b1-b30_pga.RData")
 
 # ---------------------------------------------------------------------------
 # melt pga
@@ -115,23 +87,6 @@ for (i in 800:805) { # choose which samples you want to plot
   #dev.off()
   
 } # close the i (HSInumber) loop
-
-# ----------
-# total number of samples = 1180 x 14 aspects x 7 slopes = 115,640 spectra
-# -
-# 1 sample missing (709) = 1179 samples = 115,542 spectra (p)
-# -
-# 3 configurations (imaging mistakes) removed = 115,422 spectra
-# -
-# entire 10 slope for batch 24 removed (imaging mistake) = 114,862 spectra (p_clean)
-# -
-# 159 spectra identified as I outliers removed = 114,703 spectra (p_clean_obsI)
-# -
-# 543 spectra identified as dI outliers removed = 114,160 spectra (gold)
-#
-# aspect corrected --> 106,088
-#
-# remove NA's --> 105,968
 
 # ---------------------------------------------------------------------------
 # 
